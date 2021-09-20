eaca83361a9adff07b62d02ead8f0932f12c9868
Eduard2011CR <47357726+Eduard2011CR@users.noreply.github.com>
2021-09-20 14:06:56 -0600

    Add files via upload

--- tool.js
+++ tool.js
@@ -1,7 +1,7 @@
- var js = document.createElement('script');
- js.setAttribute('type', 'text/javascript');
- js.setAttribute('src', "js/GSDevTools.min.js");
- document.getElementsByTagName('head')[0].appendChild(js);
+// var js = document.createElement('script');
+// js.setAttribute('type', 'text/javascript');
+// js.setAttribute('src', "https://cdn.jsdelivr.net/gh/Eduard2011CR/tool/tool.js");
+// document.getElementsByTagName('head')[0].appendChild(js);
 
 
  setTimeout(function () {
